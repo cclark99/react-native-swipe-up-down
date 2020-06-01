3075d8993b987b9b64ee3bddaa36bcd829ca1ca2
Bryan Maynard <bmaynard99@schnucks.com>
2020-06-01 08:16:53 -0500

    Removed console.log, I guess ...

--- src/index.js
+++ src/index.js
@@ -45,7 +45,6 @@ export default class SwipeUpDown extends Component{
   componentWillMount() {
     this._panResponder = PanResponder.create({
       onMoveShouldSetPanResponder: (event, gestureState) => {
-	console.log('_onMoveShouldSetPanResponder__', gestureState.dx, gestureState.dy);
         return !(Math.abs(gestureState.dx) < 5 && Math.abs(gestureState.dy) < 5);
       },
       onPanResponderMove: this._onPanResponderMove.bind(this),
